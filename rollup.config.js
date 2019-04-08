f4a236a3c815ca6f1881116d2d40cf15d9907a04
Josh McCall <joshmccall221@gmail.com>
2019-04-08 08:35:45 -0700

    chore(corber-react): init

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,18 @@
 import autoExternal from 'rollup-plugin-auto-external';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/rollupindex.js',
   plugins: [autoExternal()],
   output: [
-    {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
-    {file: 'lib/index.es.js', format: 'es', sourcemap: true}
+    {
+      file: 'lib/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'lib/index.es.js',
+      format: 'es',
+      sourcemap: true
+    }
   ]
 };
