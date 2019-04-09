8ab59581c5b0d732fa2aef70599195f75a6638a6
Josh McCall <joshmccall221@gmail.com>
2019-04-08 18:26:02 -0700

    wip(storybook): addons

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -6,33 +6,18 @@ import { ShimmerLoadDataExample } from './ShimmerLoadDataExample';
 import { PersonaBasicExample } from './Personas';
 import { ButtonDefaultExample } from './IButtonBasicExampleStyleProps';
 import { ShimmerApplicationExample, SeachCardExample } from './SeachCardExample';
-// import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { boolean, withKnobs, text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from "@storybook/addon-info";
-
-
+import { linkTo } from '@storybook/addon-links'
 
 // import * as ShimmerExampleStyles from './Shimmer.Example.scss';
 // example files :  https://github.com/OfficeDev/office-ui-fabric-react/tree/43e45d90f0c5cad56cf1b35c8a41361176a30b40/packages/office-ui-fabric-react/src
 
-const stories = storiesOf('office-ui-fabric', module);
-
-
-// Add the `withKnobs` decorator to add knobs support to your stories.
-// You can also configure `withKnobs` as a global decorator.
-const click = () => {
-  console.log('hey!')
-  action('button-click')()
-}
-stories.addDecorator(withKnobs)
-  .addDecorator(withInfo({
-    inline: true,
-    header: true,
-    source: false,
-    // TableComponent
-  }))
-  .add('Button - default view', () => (<Button onClick={click}>{text('Button Text', 'Click it or ticket!', '1')}</Button>))
+storiesOf('office-ui-fabric', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo({ inline: true, header: true, source: false, }))
+  .add('Button - default view', () => (<Button onClick={action('button-click')}>{text('Button Text', 'Click it or ticket!', '1')}</Button>))
   .add('shimmershimmer', () => (<> <Shimmer /> </>))
   .add('PeoplePickerTypesExampleLocal', () => (<> <PeoplePickerTypesExample /> </>))
   .add("PeoplePicker w/ delayed results", () => < PeoplePickerTypesExample delayResults={boolean('DelayedResults', true, '1')} />)
@@ -49,23 +34,11 @@ stories.addDecorator(withKnobs)
   .add("Contact Groups - loaded", () => < ShimmerApplicationExample isDataLoaded />)
 
 storiesOf("office-ui-fabric-react: Screens", module)
-  .addDecorator(withInfo({
-    inline: true,
-    header: true,
-    source: false,
-  }))
-  .add("SeachCardExample", () => < SeachCardExample {...{
-    preSelected: false,
-    image: false,
-    presence: false,
-    hidePersonaDetails: true
-  }} />)
-  .add("SeachCardExample - populated", () => < SeachCardExample {...{
-    preSelected: true,
-    image: true,
-    presence: true,
-    hidePersonaDetails: false
-  }} />)
+  .addDecorator(withInfo({ inline: true, header: true, source: false, }))
+  .add("SeachCardExample", () => < SeachCardExample {...{ preSelected: false, image: false, presence: false, hidePersonaDetails: true }} />)
+  .add("SeachCardExample - populated", () => < SeachCardExample {...{ preSelected: true, image: true, presence: true, hidePersonaDetails: false }} />)
 
-
+storiesOf('ButtonLinkTo', module)
+  .add('First', () => (<button onClick={linkTo('ButtonLinkTo', 'Second')}>Go to "Second"</button>))
+  .add('Second', () => (<button onClick={linkTo('ButtonLinkTo', 'First')}>Go to "First"</button>))
 
