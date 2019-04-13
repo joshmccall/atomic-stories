9bbd0e2d411c08fd35a90596f8550149fa2e3510
Josh McCall <joshmccall221@gmail.com>
2019-04-12 20:22:26 -0700

    fix(rollup): ts support

--- src/rollupindex.js
+++ src/rollupindex.js
@@ -1,11 +1,11 @@
-const build = require('../build/asset-manifest.json');
+// const build = require('../build/asset-manifest.json');
 // import FindYourContact from "./stories/FindYourContact";
 
 export function test() {
   // eslint-disable-next-line no-console
   console.log('test123');
   return ({
-    FindYourContact: require('./stories/FindYourContact.tsx'),
-    build
+    FindYourContact: require('./stories/FindYourContact.tsx')
+    // build
   });
 }
