25eb7ab4c7f273a1b9091001ef114f2edcfe8202
Josh McCall <joshmccall221@gmail.com>
2019-04-13 10:15:48 -0700

    feat(rollup): library exportss

--- src/rollupindex.tsx
+++ src/rollupindex.tsx
@@ -1,5 +1,7 @@
 import FindYourContact from "./stories/FindYourContact";
 
+export const test = <div> test 123!!! </div>
+
 export default FindYourContact;
 // export default function fyc() {
 //   return <FindYourContact />
