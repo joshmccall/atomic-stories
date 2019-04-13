6959cd46b59483ee0f909d611a231f73bb5061f0
Josh McCall <joshmccall221@gmail.com>
2019-04-12 19:08:40 -0700

    feat(rollup): ts support

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,60 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
-import autoExternal from 'rollup-plugin-auto-external';
+// import autoExternal from 'rollup-plugin-auto-external';
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+// import postcss from 'rollup-plugin-postcss-modules'
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
+
+import pkg from './package.json';
+
+// export default {
+//   input: 'src/rollupindex.js',
+//   plugins: [autoExternal()],
+//   output: [{
+//       file: 'lib/index.cjs.js',
+//       format: 'cjs',
+//       sourcemap: true
+//     },
+//     {
+//       file: 'lib/index.es.js',
+//       format: 'es',
+//       sourcemap: true
+//     }
+//   ]
+// };
 
 export default {
   input: 'src/rollupindex.js',
-  plugins: [autoExternal()],
   output: [
     {
-      file: 'lib/index.cjs.js',
+      file: pkg.main,
       format: 'cjs',
+      exports: 'named',
       sourcemap: true
     },
     {
-      file: 'lib/index.es.js',
+      file: pkg.module,
       format: 'es',
+      exports: 'named',
       sourcemap: true
     }
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
+    resolve(),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true
+    }),
+    commonjs()
   ]
 };
--- src/rollupindex.js
+++ src/rollupindex.js
@@ -1,5 +1,8 @@
 const build = require('../build/asset-manifest.json');
+// import FindYourContact from "./stories/FindYourContact";
 
+
+export const FindYourContact = require('./stories/FindYourContact.tsx');
 
 export function test() {
   // eslint-disable-next-line no-console
